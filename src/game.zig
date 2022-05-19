90a1a2d8a564c9b68408038955e8b7129c19a872
Juha Syrjälä <juha.syrjala@iki.fi>
2022-05-19 16:04:30 +0300

    Components velocity and position

--- src/game.zig
+++ src/game.zig
@@ -2,7 +2,8 @@ const std = @import("std");
 
 const colors = @import("colors.zig");
 const util = @import("util.zig");
-
+const vector = @import("vector.zig");
+const Vec2 = vector.Vec2;
 
 const Adapter = @import("ecs/systems.zig").Adapter;
 const System = @import("ecs/systems.zig").System;
@@ -10,60 +11,20 @@ const World = @import("ecs/systems.zig").World;
 const EntityID = @import("ecs/entities.zig").EntityID;
 const Entities = @import("ecs/entities.zig").Entities;
 
-const p = @import("player.zig");
-const Player = p.Player;
+const player = @import("player.zig");
 
 var world: World = undefined;
 
 var buffer: [5000]u8 = undefined;
 var worldAllocator = std.heap.FixedBufferAllocator.init(&buffer);
 
-fn playerInput2(player: *Player) void {
-    util.log("Player {}", .{player})  catch {};
-    const input = player.input();
-
-    if (input.changed) {
-         util.log("Input {}", .{input})  catch {};
-    }
-}
-
 pub fn setup() !void {
     try util.log("game.setup()", .{});
     colors.setup();
-
     var allocator = worldAllocator.allocator();
     world = try World.init(allocator);
+    try player.setup(allocator, &world);
 
-    const player1Id = try world.entities.new();
-    const player2Id = try world.entities.new();
-
-    var player1Component = try allocator.create(Player);
-    var player2Component = try allocator.create(Player);
-    player1Component.* = Player.init(0, player1Id);
-    player2Component.* = Player.init(1, player1Id);
-
-    try util.log("player1 {}", .{player1Component});
-    _ = player1Component;
-    _ = player2Component;
-    _ = player1Id;
-    _ = player2Id;
-
-    try world.entities.setComponent(player1Id, "player", player1Component);
-    const playerSystem = ( struct {
-        pub fn player(adapter: *Adapter) void {
-            var iter = adapter.query(&.{"player"});
-            while (iter.next()) |row| {
-                defer row.unlock();
-                if (adapter.world.entities.getComponent(row.entity, "player", *Player)) |component| {
-                    const input = component.input();
-                    if (input.changed) {
-                        util.log("input: {}", .{input}) catch {};
-                    }
-                }
-            }
-        }
-    }).player;
-    try world.register("player", playerSystem);
 
     try util.log("setup done", .{});
 }
--- src/player.zig
+++ src/player.zig
@@ -1,20 +1,47 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const vector = @import("vector.zig");
+const Vec2 = vector.Vec2;
+
 const w4 = @import("wasm4.zig");
+
+const World = @import("ecs/systems.zig").World;
 const EntityID = @import("ecs/entities.zig").EntityID;
+const Adapter = @import("ecs/systems.zig").Adapter;
 const util = @import("util.zig");
+
+
+pub fn setup(allocator: Allocator, world: *World) !void {
+    try createPlayer(allocator, world, 0, Vec2{.x = 10, .y = 130}, Vec2.zero());
+    try world.register("player", playerSystem);
+}
+
+fn createPlayer(allocator: Allocator, world: *World, index: u16, position: Vec2, velocity: Vec2) !void{
+    const player1Id = try world.entities.new();
+
+    var player1Component = try allocator.create(Player);
+    player1Component.* = Player.init(index, player1Id);
+    try world.entities.setComponent(player1Id, "player", player1Component);
+
+    var pos = try allocator.create(Vec2);
+    pos.* = position;
+    try world.entities.setComponent(player1Id, "position", pos);
+
+    var vel = try allocator.create(Vec2);
+    vel.* = velocity;
+    try world.entities.setComponent(player1Id, "velocity", vel);
+}
 
 pub const Player = struct {
     index: u16,
     entity: EntityID,
     prev_gamepad: u8,
-        counter: u32,
-
 
     pub fn init(index: u16, entity: EntityID) Player {
         return Player{
             .index = index, 
             .entity = entity, 
             .prev_gamepad = 0,
-            .counter = 1,
         };
     }
 
@@ -47,23 +74,26 @@ const Input = struct {
     changed: bool,
 };
 
-pub fn playerInput(player: *Player) void {
-    const gamepad = w4.GAMEPADS[player.index];
-    //const just_pressed = gamepad & (gamepad ^ self.prev_gamepad);
-    const changed = gamepad != player.prev_gamepad;
-    util.log("Changed:  {}, {} prev {}", .{player, gamepad, player.prev_gamepad}) catch {};
-    const input = Input{
-        .up = w4.BUTTON_UP & gamepad != 0,
-        .down = w4.BUTTON_DOWN & gamepad != 0,
-        .left = w4.BUTTON_LEFT & gamepad != 0,
-        .right = w4.BUTTON_RIGHT & gamepad != 0,
-        .button_1 = w4.BUTTON_1 & gamepad != 0,
-        .button_2 = w4.BUTTON_2 & gamepad != 0,
-        .any_button = gamepad != 0,
-        .changed = changed,
-    };
-    util.log("Input: {} gamepad: {} prev {}", .{input, gamepad, player.prev_gamepad}) catch {};
-    player.prev_gamepad = gamepad;
 
+// struct needed for closure?
+const playerSystem = ( struct {
+    pub fn playerFunc(adapter: *Adapter) void {
+        var iter = adapter.query(&.{"player", "position", "velocity"});
+        while (iter.next()) |row| {
+            defer row.unlock();
+            var player = adapter.world.entities.getComponent(row.entity, "player", *Player) orelse unreachable;
+            var position = adapter.world.entities.getComponent(row.entity, "position", *Vec2) orelse unreachable;
+            var velocity = adapter.world.entities.getComponent(row.entity, "velocity", *Vec2) orelse unreachable;
+            updatePlayer(player, position, velocity) catch |e| {
+                util.log("PlayerSystem: Failure {}", .{e}) catch {};
+            };
+        }
+    }
+}).playerFunc;
 
+fn updatePlayer(player: *Player, position: *Vec2, velocity: *Vec2) !void {
+    const input = player.input();
+    if (input.changed) {
+        try util.log("input: {} position={} velocity={}", .{input, position, velocity});
+    }
 }
